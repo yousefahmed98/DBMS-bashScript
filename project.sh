68ef1da974f463cba09e8d6afe9e76ba8795da82
yousefahmed98 <yousefa.mohamed12@gmail.com>
2022-01-18 01:19:59 +0200

    insertIntoTable done

--- project.sh
+++ project.sh
@@ -144,19 +144,40 @@ do
                                     break
                                 ;;
                                 isnertIntoTable)
+
+                                    
                                     id=-1
                                     echo "Please Enter table Name" ; 
                                     read tableName
                                     colsNum=`awk -F : 'END{print NF}' $tableName` #get the number of the cols
                                     id=`awk -F : 'END{ print $1 }' $tableName` #get the id 
                                     id=$(($id+1))
-                                    echo $id
-                                    row=()
+                                    j=1
+                                    Error=false
                                     for (( i=2; i<=$(($colsNum)); i++ )) do
-                                        echo enter value of $(awk 'BEGIN{FS=":"}{ if(NR==1)print $'$i';}' $tableName)
+                                        colType=$(awk 'BEGIN{FS=":"}{ if(NR==1)print $'$i';}' .$tableName)
+                                        echo enter value of $(awk 'BEGIN{FS=":"}{ if(NR==1)print $'$i';}' $tableName) ,type=$colType
                                         read value
+                                        if [[  $colType == "int" ]] ; then
+                                            if [[ ! $value =~ ^[0-9]+$ ]] ; then
+                                                echo "((((wrong dataType))))"
+                                                Error=true
+                                                break
+                                            fi
+                                        fi
+                                        row+=($value)
+                                        j=$(($j+1))
                                     done 
-
+                                    if [ $Error = false ] ;then
+                                        echo -n $id >> $tableName
+                                        for element in "${row[@]}"
+                                        do
+                                        : 
+                                            echo -n :$element >> $tableName
+                                        done
+                                    fi
+                                    break
+                                    
                                 ;;
                                 disconnect)
                                     disconnect=true
@@ -202,3 +223,4 @@ do
         break
     fi
 done
+
